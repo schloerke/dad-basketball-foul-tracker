e0eb8caa5ac81d4084c8173481fa40cc1b2cca83
Barret Schloerke <schloerke@gmail.com>
2017-11-07 09:32:57 -0500

    adjust starting players and name pronuciations

--- forms.js
+++ forms.js
@@ -291,15 +291,15 @@ function addCycloneRoster(sideP)
   var roster = new Array();
 
 
-roster.push( new player(0, "1" , "Madison Wise"           , 0, "F", "6-1" , "Fr", "Greenfield, Indiana"     , "Reg"));
+roster.push( new player(0, "1" , "Maddie Wise"           , 0, "F", "6-1" , "Fr", "Greenfield, Indiana"     , "Reg"));
 roster.push( new player(1, "3" , "Emily Durr"             , 0, "G", "6-0" , "Sr", "Utica, N.Y."             , "Star"));
 roster.push( new player(0, "4" , "Rae Johnson"            , 0, "G", "5-7" , "Fr", "Albertville, Minn."      , "Reg"));
 roster.push( new player(0, "10", "NEE-uh Washington"      , 0, "G", "5-7" , "So", "Stafford, Va."           , "Reg"));
-roster.push( new player(1, "12", "Hop-oh-a-tay"           , 0, "F", "6-4" , "Jr", "Brisbane, Australia"     , "Reg"));
-roster.push( new player(0, "13", "ay-dree-AH-nuh CAME-ber", 0, "F", "5-10", "So", "Lund, Sweden"            , "Reg"));
+roster.push( new player(0, "12", "Bride-ee Kennedy Hop-oh-a-tay"           , 0, "F", "6-4" , "Jr", "Brisbane, Australia"     , "Reg"));
+roster.push( new player(1, "13", "ay-dree-AH-nuh CAME-ber", 0, "F", "5-10", "So", "Lund, Sweden"            , "Reg"));
 roster.push( new player(1, "21", "Bridget Carleton"       , 0, "G", "6-1" , "Jr", "Chatham, Ontario, Canada", "Reg"));
-roster.push( new player(0, "25", "Kristin Scott"          , 0, "C", "6-3" , "Fr", "Kasson, Minn."           , "Reg"));
-roster.push( new player(1, "30", "Claire Ricketts"        , 0, "F", "6-3" , "Jr", "Parker, Texas"           , "Reg"));
+roster.push( new player(1, "25", "Kristin Scott"          , 0, "C", "6-3" , "Fr", "Kasson, Minn."           , "Reg"));
+roster.push( new player(0, "30", "Claire Ricketts"        , 0, "F", "6-3" , "Jr", "Parker, Texas"           , "Reg"));
 roster.push( new player(1, "32", "Mere Burkhall"          , 0, "F", "6-3" , "Jr", "Urbandale, Iowa"         , "Reg"));
 
 // roster.push( new player(0, "2" , "Sofija ghee-vah-leu-veech", 0, "G", "5-9" , "Fr"    , "Podgorica, Montenegro"   , "Reg"));
